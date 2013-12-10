d4cddc7a158b1084ca227d482974a1e779c8cde7
Austin King <shout@ozten.com>
2013-12-09 22:54:43 -0800

    ch01.js

--- ch01.js
+++ ch01.js
@@ -1,3 +1,4 @@
+
 function Buffer(maxWip, maxFlow) {
     var that = {};
     that.queued = 0;
@@ -39,12 +40,12 @@ function Controller(kp, ki) {
 
     that.work = function(e) {
         that.i += e;
-        //console.log(that.kp, '*', e, '+', that.ki, '*', that.i, (that.kp * e) + (that.ki * that.i));
+        console.log(that.kp, '*', e, '+', that.ki, '*', that.i, (that.kp * e) + (that.ki * that.i));
         return (that.kp * e) + (that.ki * that.i);
     };
     return that;
 }
-
+var data = [];
 function openLoop(p, tm) {
     tm = tm || 5000;
     var that = {};
@@ -56,7 +57,7 @@ function openLoop(p, tm) {
         var u = that.target(t);
         var y = p.work(u);
 
-        console.log(t, u, 0, u, y);
+        data.push([t, u, 0, u, y].join(' '));
     }
     return that;
 }
@@ -70,6 +71,19 @@ function closedLoop(c, p, tm) {
             return 0.0;
         } else if (t < 300) {
             return 50.0;
+
+        } else if (t < 600) {
+            return 250.0;
+
+        } else if (t < 1300) {
+            return 150.0;
+
+        } else if (t < 2500) {
+            return 0.0;
+        } else if (t < 4500) {
+            return 150.0;
+
+
         } else {
             return 10.0;
         }
@@ -79,16 +93,38 @@ function closedLoop(c, p, tm) {
     for (var t=0; t < tm; t++) {
         var r = that.setpoint(t);
         var e = r - y;
+        //console.log('setpoint=', r, 'y=', y, 'error=', e);
         u = c.work(e);
         y = p.work(u);
-
-        console.log(t, r, e, u, y);
+console.log('doing data');
+        data.push([t, r, e, u, y].join(' '));
+        console.log(t + ' ' + tm);
     }
     return that;
 }
 
-var c = Controller(1.25, 0.1);
-var p = Buffer(50, 10);
+document.getElementById('slider').addEventListener('change', function(e) {
+    console.log('yo ho');
+    var load;
+    if (e.value) {
+        load = parseFloat(e.value);
+    } else {
+        load = parseFloat(e.target.value);
+    }
+console.log(load);
+    console.log('Controller load=', load);
 
-//openLoop(p, 1000);
-closedLoop(c, p, 1000);
+    var c = Controller(load, 0.1);
+    console.log('created controller');
+    var p = Buffer(500, 250);
+console.log('created buffer');
+    // reset data
+    data = [];
+    //openLoop(p, 1000);
+    closedLoop(c, p, 1000);
+    console.log('after closedLoop');
+    console.log(data.join('\n'));
+    gnuplot.putFile('simulation.out', data.join('\n'));
+    gnuplot.onOutput(data.join('\n'));
+    runScript();
+}, false);
